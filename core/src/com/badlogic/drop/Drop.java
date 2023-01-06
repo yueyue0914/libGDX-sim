289781be389b26d4bb5b8ba592e1e9d8e96cbe9d
yueyue0914 <970914cai@gmail.com>
2023-01-06 16:09:10 +0800

    libGDX初始项目提交

---
+++ core/src/com/badlogic/drop/Drop.java
@@ -0,0 +1,67 @@
+package com.badlogic.drop;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector3;
+
+public class Drop extends ApplicationAdapter {
+	private Texture dropImage;
+	private Texture bucketImage;
+	private Sound dropSound;
+	private Music rainMusic;
+	private OrthographicCamera camera;
+	private SpriteBatch batch;
+	private Rectangle bucket;
+	
+	@Override
+	public void create () {
+		// 加载雨滴和桶的资源图片，每个像素大小为64*64
+		dropImage = new Texture(Gdx.files.internal("droplet.png"));
+		bucketImage = new Texture(Gdx.files.internal("bucket.png"));
+
+		// 加载雨滴音效和下雨的背景声音
+		dropSound = Gdx.audio.newSound(Gdx.files.internal("drop.wav"));
+		rainMusic = Gdx.audio.newMusic(Gdx.files.internal("rain.mp3"));
+
+		// 循环播放背景音乐
+		rainMusic.setLooping(true);
+		rainMusic.play();
+
+		// ... 其他代码块 ...
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 800, 480);
+
+		batch = new SpriteBatch();
+
+		bucket = new Rectangle();
+		bucket.x = 800 / 2 - 64 / 2;
+		bucket.y = 20;
+		bucket.width = 64;
+		bucket.height = 64;
+
+	}
+
+	@Override
+	public void render () {
+		ScreenUtils.clear(0, 0, 0.2f, 1);
+		camera.update();
+		batch.setProjectionMatrix(camera.combined);
+		batch.begin();
+		batch.draw(bucketImage, bucket.x, bucket.y);
+		batch.end();
+		if(Gdx.input.isTouched()) {
+			Vector3 touchPos = new Vector3();
+			touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+			camera.unproject(touchPos);
+			bucket.x = touchPos.x - 64 / 2;
+		}
+	}
+
+}
