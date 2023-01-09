8f94fd75f2c2522691cfd5e3def2849f1ea5d047
yueyue0914 <970914cai@gmail.com>
2023-01-09 08:46:23 +0800

    雨滴渲染、import整理

--- core/src/com/badlogic/drop/Drop.java
+++ core/src/com/badlogic/drop/Drop.java
@@ -1,33 +1,63 @@
 package com.badlogic.drop;
 
+import java.util.Iterator;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
-import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
-import com.badlogic.gdx.Input;//监测键盘输入的
 import com.badlogic.gdx.utils.Array;
-import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.TimeUtils;
-
-
-
 
 public class Drop extends ApplicationAdapter {
 	private Texture dropImage;
 	private Texture bucketImage;
 	private Sound dropSound;
 	private Music rainMusic;
-	private OrthographicCamera camera;
 	private SpriteBatch batch;
+	private OrthographicCamera camera;
 	private Rectangle bucket;
 	private Array<Rectangle> raindrops;
 	private long lastDropTime;
+
+	@Override
+	public void create() {
+		// 加载液滴和桶的图像，分别为64x64像素
+		dropImage = new Texture(Gdx.files.internal("droplet.png"));
+		bucketImage = new Texture(Gdx.files.internal("bucket.png"));
+
+		// 加载雨滴音效和雨滴背景“音乐”
+		dropSound = Gdx.audio.newSound(Gdx.files.internal("drop.wav"));
+		rainMusic = Gdx.audio.newMusic(Gdx.files.internal("rain.mp3"));
+
+		//立即开始播放背景音乐
+		rainMusic.setLooping(true);
+		rainMusic.play();
+
+		// 创建相机和SpriteBatch
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 800, 480);
+		batch = new SpriteBatch();
+
+		// 创建一个矩形逻辑上表示桶
+		bucket = new Rectangle();
+		bucket.x = 800 / 2 - 64 / 2; // 将桶水平居中
+		bucket.y = 20; // 桶的左下角比屏幕底部边缘高出20个像素
+		bucket.width = 64;
+		bucket.height = 64;
+
+		// 创建雨滴数组并生成第一个雨滴
+		raindrops = new Array<Rectangle>();
+		spawnRaindrop();
+	}
+
 	private void spawnRaindrop() {
 		Rectangle raindrop = new Rectangle();
 		raindrop.x = MathUtils.random(0, 800-64);
@@ -37,63 +67,62 @@ public class Drop extends ApplicationAdapter {
 		raindrops.add(raindrop);
 		lastDropTime = TimeUtils.nanoTime();
 	}
-	
-	@Override
-	public void create () {
-		// 加载雨滴和桶的资源图片，每个像素大小为64*64
-		dropImage = new Texture(Gdx.files.internal("droplet.png"));
-		bucketImage = new Texture(Gdx.files.internal("bucket.png"));
-
-		// 加载雨滴音效和下雨的背景声音
-		dropSound = Gdx.audio.newSound(Gdx.files.internal("drop.wav"));
-		rainMusic = Gdx.audio.newMusic(Gdx.files.internal("rain.mp3"));
-
-		// 循环播放背景音乐
-		rainMusic.setLooping(true);
-		rainMusic.play();
-
-		// ... 其他代码块 ...
-		camera = new OrthographicCamera();
-		camera.setToOrtho(false, 800, 480);
-
-		batch = new SpriteBatch();
-
-		bucket = new Rectangle();
-		bucket.x = 800 / 2 - 64 / 2;
-		bucket.y = 20;
-		bucket.width = 64;
-		bucket.height = 64;
-
-		//键盘控制移动
-		if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) bucket.x -= 200 * Gdx.graphics.getDeltaTime();
-		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) bucket.x += 200 * Gdx.graphics.getDeltaTime();
-
-		//限制水桶位置
-		if(bucket.x < 0) bucket.x = 0;
-		if(bucket.x > 800 - 64) bucket.x = 800 - 64;
-
-		//水滴
-		raindrops = new Array<Rectangle>();
-		spawnRaindrop();
-
-	}
 
 	@Override
-	public void render () {
+	public void render() {
+		// 用深蓝色清除屏幕。要清除的参数是用于清除屏幕的颜色[0,1]范围内的红色、绿色、蓝色和alpha组件。
 		ScreenUtils.clear(0, 0, 0.2f, 1);
+
+		// 告诉相机更新它的矩阵。
 		camera.update();
+
+		//告诉SpriteBatch在相机指定的坐标系统中渲染。
 		batch.setProjectionMatrix(camera.combined);
+
+		// 开始新的一批，画桶和所有的水滴
 		batch.begin();
 		batch.draw(bucketImage, bucket.x, bucket.y);
+		for(Rectangle raindrop: raindrops) {
+			batch.draw(dropImage, raindrop.x, raindrop.y);
+		}
 		batch.end();
+
+		// 加载键盘输入
 		if(Gdx.input.isTouched()) {
 			Vector3 touchPos = new Vector3();
 			touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
 			camera.unproject(touchPos);
 			bucket.x = touchPos.x - 64 / 2;
 		}
-		//渲染雨滴
+		if(Gdx.input.isKeyPressed(Keys.LEFT)) bucket.x -= 200 * Gdx.graphics.getDeltaTime();
+		if(Gdx.input.isKeyPressed(Keys.RIGHT)) bucket.x += 200 * Gdx.graphics.getDeltaTime();
+
+		// 保证桶在屏幕内
+		if(bucket.x < 0) bucket.x = 0;
+		if(bucket.x > 800 - 64) bucket.x = 800 - 64;
+
+		// 监测渲染雨滴
 		if(TimeUtils.nanoTime() - lastDropTime > 1000000000) spawnRaindrop();
+
+		// 移动雨滴，删除任何在屏幕底部边缘以下或击中桶的雨滴。在后一种情况下，我们也会播放音效。
+		for (Iterator<Rectangle> iter = raindrops.iterator(); iter.hasNext(); ) {
+			Rectangle raindrop = iter.next();
+			raindrop.y -= 200 * Gdx.graphics.getDeltaTime();
+			if(raindrop.y + 64 < 0) iter.remove();
+			if(raindrop.overlaps(bucket)) {
+				dropSound.play();
+				iter.remove();
+			}
+		}
 	}
 
+	@Override
+	public void dispose() {
+		//处理所有本机资源
+		dropImage.dispose();
+		bucketImage.dispose();
+		dropSound.dispose();
+		rainMusic.dispose();
+		batch.dispose();
+	}
 }
